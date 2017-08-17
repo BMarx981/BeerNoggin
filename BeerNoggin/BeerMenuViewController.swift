6204ec11d7f957ee666bf9555ea768f122835834
Brian Marx <b.marx981@gmail.com>
2017-08-17 07:11:50 -0400

    Add location retrival functionality :

--- BeerNoggin/BeerMenuViewController.swift
+++ BeerNoggin/BeerMenuViewController.swift
@@ -12,15 +12,22 @@ import CoreLocation
 let reuseId = "beerId"
 class BeerMenuViewController: UICollectionViewController, CLLocationManagerDelegate {
 
-    var beerList = [Beer]()
+    //MARK: - Constants
     let beer = Beer()
+    let locationManager = CLLocationManager()
     
+    //MARK: - Variables
+    var beerList = [Beer]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView?.backgroundView = UIImageView(image: #imageLiteral(resourceName: "White tile"))
         populateBeerList()
-        let locationManager = CLLocationManager()
+        
         locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -30,24 +37,34 @@ class BeerMenuViewController: UICollectionViewController, CLLocationManagerDeleg
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: - CollectionViewDataSource
+    //*****************************************************//
+
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection: Int) -> Int {
+        
         return beerList.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseId, for: indexPath)
+        
         let imageView = UIImageView()
         imageView.image = #imageLiteral(resourceName: "BlackBoard")
         cell.contentView.sendSubview(toBack: imageView)
+        
         let beerNameLabel = cell.viewWithTag(10) as! UILabel
         beerNameLabel.text = beerList[indexPath.item].name //Name Label
+        
         let breweryLabel = cell.viewWithTag(20) as! UILabel
         breweryLabel.text = beerList[indexPath.item].brewery  //Brewery label
+        
         let styleLabel = cell.viewWithTag(22) as! UILabel
         styleLabel.text = beerList[indexPath.item].style  //Style Label
+        
         let alcohol = cell.viewWithTag(30) as! UILabel
         alcohol.text = String(beerList[indexPath.item].alcohol) // Alcohol Label
+        
         let descriptionLabel = cell.viewWithTag(40) as! UILabel
         descriptionLabel.text = beerList[indexPath.item].descriptionOf //Description Label
         
@@ -59,19 +76,38 @@ class BeerMenuViewController: UICollectionViewController, CLLocationManagerDeleg
         
     }
     
+    //MARK: - Dummy data
     func populateBeerList() {
-        var num = 0
-        for _ in 0...11 {
+
+        for i in 0...11 {
+            
             var beer = Beer()
-            beer.name = "Beer #\(num)"
-            beer.brewery = "Brewery #\(num)"
-            beer.style = "Style Type \(num)"
-            beer.alcohol = Double(num)
-            beer.descriptionOf = "Description #\(num)"
-            num += 1
+            beer.name = "Beer #\(i)"
+            beer.brewery = "Brewery #\(i)"
+            beer.style = "Style Type \(i)"
+            beer.alcohol = Double(i)
+            beer.descriptionOf = "Description #\(i)"
             beerList.append(beer)
+        }
+    }
+    
+    
+    //MARK: - Location Manager
+    //*****************************************************//
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[locations.count - 1]
+        
+        if location.horizontalAccuracy > 0 {
+            
+            locationManager.stopUpdatingLocation()
             
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+        print(error)
     }
 
 }
